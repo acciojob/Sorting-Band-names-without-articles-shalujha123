489e802cefe672e64fd778f1a52e3c8e0fb8255c
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-25 22:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+  function stripArticle(name) {
+    return name.replace(/^(a |an |the )/i, '').trim();
+  }
+
+  touristSpots.sort((a, b) => {
+    let nameA = stripArticle(a).toLowerCase();
+    let nameB = stripArticle(b).toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+
+  const ul = document.getElementById("band");
+
+  touristSpots.forEach(spot => {
+    const li = document.createElement("li");
+    li.textContent = spot;
+    ul.append(li);
+  });
